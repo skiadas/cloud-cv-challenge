f50412c90bd9a3af90bedaadf12d0408b9691349
Haris Skiadas <cskiadas@gmail.com>
2022-07-24 17:22:35 -0400

    Expose only distribution interface. Link bucket properly

--- cdk/lib/distro-stack.ts
+++ cdk/lib/distro-stack.ts
@@ -1,5 +1,5 @@
 import { Stack, StackProps, Tags, RemovalPolicy } from 'aws-cdk-lib';
-import { Distribution } from 'aws-cdk-lib/aws-cloudfront';
+import { Distribution, IDistribution } from 'aws-cdk-lib/aws-cloudfront';
 import { S3Origin } from 'aws-cdk-lib/aws-cloudfront-origins';
 import { Bucket, BlockPublicAccess, IBucket } from 'aws-cdk-lib/aws-s3';
 import { BucketDeployment, Source } from 'aws-cdk-lib/aws-s3-deployment';
@@ -9,7 +9,7 @@ import * as path from 'path';
 // Stack that sets up the cloudfront distribution and related items
 export class DistroStack extends Stack {
   public readonly bucket: IBucket;
-  public readonly distribution: Distribution;
+  public readonly distribution: IDistribution;
 
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
@@ -29,7 +29,7 @@ export class DistroStack extends Stack {
     this.bucket = deployment.deployedBucket;
 
     const distribution = new Distribution(this, 'cloudfrontDistro', {
-      defaultBehavior: { origin: new S3Origin(this.bucket) }
+      defaultBehavior: { origin: new S3Origin(staticPages) }
     });
 
     this.distribution = distribution;
